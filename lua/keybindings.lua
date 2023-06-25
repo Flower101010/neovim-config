3baab80507538f05e4a451ee0d9c5110aacf9f97
Flower <logic.huahaoyu@gmail.com>
2023-06-25 12:37:50 +0800

    change

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,8 +1,35 @@
 -- 保存本地变量
 local map = vim.api.nvim_set_keymap
 local opt = {noremap = true, silent = true }
+-- 浏览代码
+map("n", "<C-u>", "9k", opt)
+map("n", "<C-d>", "9j", opt)
+
+-- visual 模式下可以连续 > 缩进代码
+map('v', '<', '<gv', opt)
+map('v', '>', '>gv', opt)
+
+-- alt + hjkl 在窗口之间跳转
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+
+-- 分屏相关
+map("n", "sv", ":vsp<CR>", opt)
+map("n", "sh", ":sp<CR>", opt)
+map("n", "sc", "<C-w>c", opt)
+map("n", "so", "<C-w>o", opt) -- close others
+-- 比例控制（不常用，因为支持鼠标拖拽）
+map("n", "s>", ":vertical resize +20<CR>", opt)
+map("n", "s<", ":vertical resize -20<CR>", opt)
+map("n", "s=", "<C-w>=", opt)
+map("n", "sj", ":resize +10<CR>",opt)
+map("n", "sk", ":resize -10<CR>",opt)
+
 -- nvimTree
 map('n', '<F3>', ':NvimTreeToggle<CR>', opt)
+
 -- bufferline 左右Tab切换
 map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
 map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
