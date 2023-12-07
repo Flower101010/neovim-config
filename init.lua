5738bda8b7d5d7304628674d39b75abd9f477cb5
Flower <logic.huahaoyu@gmail.com>
2023-12-07 21:28:32 +0800

    更改字体

--- init.lua
+++ init.lua
@@ -1,19 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- 设置 shell 为 powershell.exe
-vim.o.shell = "pwsh.exe"
-
--- 设置 shellxquote
-vim.o.shellxquote = ""
-
--- 设置 shellcmdflag
-vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
-
--- 设置 shellquote
-vim.o.shellquote = ""
-
--- 设置 shellpipe
-vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
-
--- 设置 shellredir
-vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,26 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local opt = vim.opt
+vim.opt.guifont = "UbuntuMono Nerd Font:h14"
 
-opt.guifont = "UbuntuMono Nerd Font:h14"
+-- 设置 shell 为 powershell.exe
+vim.o.shell = "pwsh.exe"
+
+-- 设置 shellxquote
+vim.o.shellxquote = ""
+
+-- 设置 shellcmdflag
+vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
+
+-- 设置 shellquote
+vim.o.shellquote = ""
+
+-- 设置 shellpipe
+vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
+
+-- 设置 shellredir
+vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
+
+if vim.g.neovide then
+  vim.g.neovide_fullscreen = true
+end
