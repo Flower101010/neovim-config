8ed8d265b60ead99f949b5e69e120a75c3fd6635
Flower <logic.huahaoyu@gmail.com>
2024-01-24 23:30:55 +0800

    添加快捷键使之能够控制neovide的字体大小

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 if vim.g.neovide then
+  -- <F11> 控制全屏
   vim.api.nvim_set_keymap(
     "n",
     "<F11>",
     [[:lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>]],
     { noremap = true, silent = true }
   )
+  -- "<C-=>" 放大字体 "<C-->" 缩小字体 "<C-0>" 恢复默认
+  vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
 end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,30 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.guifont = "UbuntuMono Nerd Font:h14"
-
--- 设置 shell 为 powershell.exe
-vim.o.shell = "pwsh.exe"
-
--- 设置 shellxquote
-vim.o.shellxquote = ""
-
--- 设置 shellcmdflag
-vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
-
--- 设置 shellquote
-vim.o.shellquote = ""
-
--- 设置 shellpipe
-vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
-
--- 设置 shellredir
-vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
-
 -- 禁用拼写检查
 vim.opt.spell = false
 
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "JetBrainsMono Nerd Font:h13"
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_refresh_rate = 165
