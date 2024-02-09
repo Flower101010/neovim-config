59abfd7f01d47dc24e79e8cde1c645310845d363
Flower <logic.huahaoyu@gmail.com>
2024-02-09 14:57:50 +0800

    更改字体 添加主题

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,20 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.guifont = "JetBrainsMono Nerd Font:h13"
--- 禁用拼写检查
-vim.opt.spell = false
--- 禁用鼠标
-vim.o.mouse = ""
+vim.o.guifont = "JetBrainsMono Nerd Font,Source Han Sans SC VF,Source Han Sans VF:h13"
 
 -- 设置窗口透明度
 vim.o.winblend = 50
 -- 设置弹出菜单透明度
 vim.o.pumblend = 80
+-- 禁用拼写检查
+vim.opt.spell = false
 
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+  vim.o.guifont = "JetBrainsMono Nerd Font,Source Han Sans SC VF,Source Han Sans VF:h13"
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_refresh_rate = 165
