e32bd9543be8c762f567e231aa8db397bd2f2710
Flower <logic.huahaoyu@gmail.com>
2024-04-04 17:26:51 +0800

    Disable Copilot fpr some filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -13,6 +13,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
-
--- Copilot is disabled by default
-vim.cmd("silent! Copilot disable")
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,21 @@
+if true then
+  return {
+    {
+      "zbirenbaum/copilot.lua",
+      opts = {
+        filetypes = {
+          markdown = false,
+          help = false,
+          yaml = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          tex = false,
+          ["."] = false,
+        },
+      },
+    }
+  }
+end
