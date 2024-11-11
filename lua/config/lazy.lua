300c3e311a1607712ebfc225399446a216a72030
Flos Hua <logic.huahaoyu@gmail.com>
2024-11-11 23:15:44 +0800

    chore: Remove Copilot plugin import from lazy configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim",                                    import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.editor.trouble-v3" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
